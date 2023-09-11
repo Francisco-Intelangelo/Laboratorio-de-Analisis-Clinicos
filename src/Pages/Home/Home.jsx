592938c426453ecbe99c2edd2e5fca46c1884202
francisco.i <franciscointelangelo@gmail.com>
2023-09-11 11:22:39 -0300

    creacion del accordion y cambio de botones del home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,17 +4,45 @@ import uno from "../../assets/AssetsHome/Services/uno.png"
 import dos from "../../assets/AssetsHome/Services/dos.png"
 import tres from "../../assets/AssetsHome/Services/tres.png"
 import "./Home.css"
+import Accordion from '../../Components/Accordion/Accordion';
+
+const accordionData = [
+    {
+        title: "Pregunta 1",
+        content: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                In iste quibusdam rem quidem veniam explicabo, quia ratione hic 
+                officia, inventore magnam autem ipsam perferendis, saepe quaerat 
+                quod voluptates? Quas, eaque.`,
+    },
+    {
+        title: "Pregunta 2",
+        content: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                In iste quibusdam rem quidem veniam explicabo, quia ratione hic 
+                officia, inventore magnam autem ipsam perferendis, saepe quaerat 
+                quod voluptates? Quas, eaque.`,
+    },
+    {
+        title: "Pregunta 3",
+        content: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                In iste quibusdam rem quidem veniam explicabo, quia ratione hic 
+                officia, inventore magnam autem ipsam perferendis, saepe quaerat 
+                quod voluptates? Quas, eaque.`,
+    },
+];
 
 export const Home = () => {
-    
     return (
         <div>
             <div className="containerHome">
                 <section className="banner">
                     <Slider/>
                     <div className="buttons">
-                        <li className="sacar-turno"><NavLink to="/estudios">SACAR TURNO</NavLink></li>
-                        <li className="ver-resultados"><NavLink>VER RESULTADOS</NavLink></li>
+                        <li className="sacar-turno">
+                            <NavLink to="/estudios">SACAR TURNO</NavLink>
+                        </li>
+                        <li className="ver-resultados">
+                            <NavLink to="/ingresar">VER RESULTADOS</NavLink>
+                        </li>
                     </div>
                 </section>
                 <section className="servicios">
@@ -25,7 +53,8 @@ export const Home = () => {
                     </div>
                 </section>
                 <section className="preguntas">
-                <h3 className='title_accordion'>Preguntas Frecuentes</h3>
+                    <h3 className='title_accordion'>Preguntas Frecuentes</h3>
+                    <Accordion sections = {accordionData} />
                 </section>
             </div>
         </div>
