a0c22b5908fae7c124dab2e3f919273887cda5c8
francisco.i <franciscointelangelo@gmail.com>
2023-09-21 10:25:21 -0300

    se agregaron mas analisis y cambio de estilos

--- src/Pages/Estudios/Estudios.jsx
+++ src/Pages/Estudios/Estudios.jsx
@@ -1,7 +1,6 @@
 import "./Estudios.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faCaretDown,
   faHouse,
   faTeletype,
 } from "@fortawesome/free-solid-svg-icons";
@@ -13,32 +12,55 @@ export const Estudios = () => {
         <h1 className="estudios_title">¿Que estudios realizamos?</h1>
         <div className="estudios_container">
           <div className="estudio_card">
-            <p>Hemograma</p>
-            <FontAwesomeIcon icon={faCaretDown} />
+            <p className="text_card">Hemograma</p>
           </div>
           <div className="estudio_card">
-            <p>LDL</p>
-            <FontAwesomeIcon icon={faCaretDown} />
+            <p className="text_card">LDL</p>
           </div>
           <div className="estudio_card">
-            <p>HDL</p>
-            <FontAwesomeIcon icon={faCaretDown} />
+            <p className="text_card">HDL</p>
           </div>
           <div className="estudio_card">
-            <p>TSH</p>
-            <FontAwesomeIcon icon={faCaretDown} />
+            <p className="text_card">TSH</p>
           </div>
           <div className="estudio_card">
-            <p>Urocultivo</p>
-            <FontAwesomeIcon icon={faCaretDown} />
+            <p className="text_card">Urocultivo</p>
           </div>
           <div className="estudio_card">
-            <p>Glucosa</p>
-            <FontAwesomeIcon icon={faCaretDown} />
+            <p className="text_card">Glucosa</p>
           </div>
           <div className="estudio_card">
-            <p>Voriconazol</p>
-            <FontAwesomeIcon icon={faCaretDown} />
+            <p className="text_card">Voriconazol</p>
+          </div>
+          <div className="estudio_card">
+            <p className="text_card">Acetona</p>
+          </div>
+          <div className="estudio_card">
+            <p className="text_card">Acido Base</p>
+          </div>
+          <div className="estudio_card">
+            <p className="text_card">Ciclosporina</p>
+          </div>
+          <div className="estudio_card">
+            <p className="text_card">Elastasa</p>
+          </div>
+          <div className="estudio_card">
+            <p className="text_card">Factor II</p>
+          </div>
+          <div className="estudio_card">
+            <p className="text_card">Cortisol Vespertino</p>
+          </div>
+          <div className="estudio_card">
+            <p className="text_card">Inhibina B</p>
+          </div>
+          <div className="estudio_card">
+            <p className="text_card">Iodo - Orina</p>
+          </div>
+          <div className="estudio_card">
+            <p className="text_card">Microalbuminuria</p>
+          </div>
+          <div className="estudio_card">
+            <p className="text_card">Lipasa</p>
           </div>
         </div>
         <div className="estudios_solicitaTurno">
@@ -52,27 +74,19 @@ export const Estudios = () => {
                   icon={faTeletype}
                   style={{ fontSize: "100px" }}
                 />
-                <p>Atención telefónica</p>
-                <p>Lunes a viernes</p>
-                <p>08:00 - 16:00</p>
+                <p className="text_horarios">Atención telefónica<br/>Lunes a viernes<br/>08:00 - 16:00</p>
               </div>
               <div className="estudios_solicitaTurno_white_cont">
                 <FontAwesomeIcon icon={faHouse} style={{ fontSize: "100px" }} />
-                <p>Atención en sede <br/>José Baigorrí 623</p>
-                <p>Lunes a viernes</p>
-                <p>08:00 - 16:00</p>
+                <p className="text_horarios">Atención en sede <br/>José Baigorrí 623<br/>Lunes a viernes<br/>08:00 - 16:00</p>
               </div>
               <div className="estudios_solicitaTurno_white_cont">
                 <FontAwesomeIcon icon={faHouse} style={{ fontSize: "100px" }} />
-                <p>Atención en sede <br/>Av. General Paz 272</p>
-                <p>Lunes y Miercoles</p>
-                <p>08:00 - 17:00</p>
+                <p className="text_horarios">Atención en sede <br/>Av. General Paz 272<br/>Lunes y Miercoles<br/>08:00 - 17:00</p>
               </div>
               <div className="estudios_solicitaTurno_white_cont">
                 <FontAwesomeIcon icon={faHouse} style={{ fontSize: "100px" }} />
-                <p>Atención en sede <br/>Viamonte 581</p>
-                <p>Lunes a viernes</p>
-                <p>08:00 - 16:00</p>
+                <p className="text_horarios">Atención en sede <br/>Viamonte 581<br/>Lunes a viernes<br/>08:00 - 16:00</p>
               </div>
             </div>
           </div>
